ea7ec45263cd81ddd1d1d7f02b33dd6253abbcc5
ganeshpawar515 <ganeshp.py07@gmail.com>
2025-02-05 13:34:25 +0530

    updated settings according to pythonanywhere requirements

--- myProject/settings.py
+++ myProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-^=llwt18ubkj0y4md1ek05mn^l%@*k97^m*68bz62rzyoi(rda
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,7 +119,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# This is required for PythonAnywhere
+STATIC_ROOT = "/home/ganeshpawar1/mySite/Django-rest/static_root"
+
+# If you have extra static files (optional)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_root')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
